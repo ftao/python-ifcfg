f7a64441417a5c1207bfbbff221fc0d832847d0a
Benjamin Bach <benjamin@overtag.dk>
2017-07-20 16:29:35 +0200

    Move pkg to src/ folder structure, use Tox for testing, add python 3.4 and 3.5 tests

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-
+import os
+import sys
 from setuptools import setup, find_packages
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 import ifcfg
 
@@ -8,13 +11,14 @@ setup(
     version=ifcfg.__version__,
     description="Python Ifconfig Wrapper for Unix/Linux/MacOSX",
     long_description="Python Ifconfig Wrapper for Unix/Linux/MacOSX",
-    classifiers=[], 
+    classifiers=[],
     keywords='',
     author='Original author: BJ Dierkes',
     author_email='info@learningequality.org',
     url='https://github.com/learningequality/python-ifcfg',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     test_suite='nose.collector',
