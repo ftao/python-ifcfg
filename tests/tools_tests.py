7be79e544eecf158a6ff1bde2f9f76f5145e4ae1
Benjamin Bach <benjamin@overtag.dk>
2017-08-01 12:26:18 +0200

    Add a test that calls exec_cmd

--- tests/tools_tests.py
+++ tests/tools_tests.py
@@ -5,6 +5,7 @@ import os
 import unittest
 
 import ifcfg
+from ifcfg.tools import exec_cmd
 from nose.tools import eq_
 
 
@@ -15,3 +16,7 @@ class IfcfgToolsTestCase(unittest.TestCase):
         log = ifcfg.tools.minimal_logger(__name__)
         eq_(log.level, logging.DEBUG)
         os.environ['IFCFG_DEBUG'] = '0'
+
+    def test_command(self):
+        output, __, __ = exec_cmd("echo -n 'this is a test'")
+        self.assertEqual(output, "this is a test")
