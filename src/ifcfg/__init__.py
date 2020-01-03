01ab28dfb2ac16cb25be866d6ee7230f9674e2b9
Loy van Beek <loy.vanbeek@mojin-robotics.de>
2020-01-03 15:05:38 +0100

    Reword error message a bit

--- src/ifcfg/__init__.py
+++ src/ifcfg/__init__.py
@@ -25,7 +25,7 @@ def get_parser_class():
         if not os.path.exists(Parser.get_command()[0]):
             Parser = parser.UnixIPParser
             if not os.path.exists(Parser.get_command()[0]):
-                Log.warning("Neither `ifconfig` nor `ip` commands are available, getting interfaces is likely to fail")
+                Log.warning("Neither `ifconfig` nor `ip` commands are available, listing network interfaces is likely to fail")
     elif distro in ['Darwin', 'MacOSX']:
         Parser = parser.MacOSXParser
     elif distro == 'Windows':
